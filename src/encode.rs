cfc77750b26c998cb2e1d99ebd80f5d12831e3fa
Shun Sakai <sorairolake@protonmail.ch>
2024-11-07 15:04:30 +0900

    chore: Use `const`

--- src/encode.rs
+++ src/encode.rs
@@ -182,16 +182,16 @@ impl<W: Write> image::ImageEncoder for Encoder<W> {
             ExtendedColorType, ImageError,
         };
 
-        let name = "image";
+        const NAME: &str = "image";
         match color_type {
             ExtendedColorType::L1 => self
-                .encode(buf, name, width, height, None, None)
+                .encode(buf, NAME, width, height, None, None)
                 .map_err(ImageError::IoError),
             ExtendedColorType::L8 => {
                 let mut buf = buf.to_vec();
                 buf.iter_mut()
                     .for_each(|p| *p = u8::from(*p <= (u8::MAX / 2)));
-                self.encode(buf, name, width, height, None, None)
+                self.encode(buf, NAME, width, height, None, None)
                     .map_err(ImageError::IoError)
             }
             _ => Err(ImageError::Encoding(EncodingError::new(
