5fb7fe65f10ce5408975b56d2ee8962097aabc4e
Shun Sakai <sorairolake@protonmail.ch>
2025-12-16 01:51:45 +0900

    chore: Remove `inline` attribute

--- src/encode.rs
+++ src/encode.rs
@@ -29,7 +29,6 @@ impl<W: Write> Encoder<W> {
     /// let buf = [].as_mut_slice();
     /// let encoder = Encoder::new(buf);
     /// ```
-    #[inline]
     pub const fn new(writer: W) -> Self {
         Self { writer }
     }
